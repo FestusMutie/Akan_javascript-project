a0526bf5aaf4afda0adaf8c12c6594e8f6f2cd0c
FestusMutie <mutiefestus84@gmail.com>
2021-04-11 22:07:39 +0300

    set else if condition for gender and result

--- index.js
+++ index.js
@@ -12,8 +12,33 @@ function dateAkan(){
   var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   if (gender =="male" && result == 0){
       alert(males[0]);
-      
-  }
+}
+else if (gender == "male" && result == 1) {
+    alert(males[1]);
+  } else if (gender == "male" && result == 2) {
+    alert(males[2]);
+  } else if (gender == "male" && result == 3) {
+    alert(males[3]);
+  } else if (gender == "male" && result == 4) {
+    alert(males[4]);
+  } else if (gender == "male" && result == 5) {
+    alert(males[5]);
+  } else if (gender == "male" && result == 6) {
+    alert(males[6]);
+  } else if (gender == "female" && result == 0) {
+    alert(females[0]);
+  } else if (gender == "female" && result == 1) {
+    alert(females[1]);
+  } else if (gender == "female" && result == 2) {
+    alert(females[2]);
+  } else if (gender == "female" && result == 3) {
+    alert(females[3]);
+  } else if (gender == "female" && result == 4) {
+    alert(females[4]);
+  } else if (gender == "female" && result == 5) {
+    alert(females[5]);
+  } else if (gender == "female" && result == 6) {
+    alert(females[6]);
     
     
     
