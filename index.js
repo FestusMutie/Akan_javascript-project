65883d69e63119033797b4b7453c284c837299dd
FestusMutie <mutiefestus84@gmail.com>
2021-04-11 20:48:12 +0300

    set variable for century digits

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function day (CC,YY,MM,DD){
     return((((cc/19)+1*cc-1)+((5*yy/4))+((26*(mm+1)/10))+ dd)%7)+1
 }
-
+function dateAkan(){
+    var cc =(document.getElementById("Id1").value);
+    
 }
