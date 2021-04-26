27e10d6d9ca08136c4361135040b4df8335f5646
FestusMutie <mutiefestus84@gmail.com>
2021-04-26 07:58:39 +0300

    update javascript code

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
-function day (CC,YY,MM,DD){
-    return((((cc/19)+1*cc-1)+((5*yy/4))+((26*(mm+1)/10))+ dd)%7)+1
-}
-function dateAkan(){
-    var cc =(document.getElementById("Id1").value);
-    var yy =(document.getElementById("Id2").value);
-    var mm =(document.getElementById("Id3").value);
-    var dd =(document.getElementById("Id4").value); 
-    var gender =(document.getElementById("Id5").value);
-    var result =day(cc,yy,mm,dd).toFixed();
-    var males = ["Kwasi", "kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  if (gender =="male" && result == 0){
-      alert(males[0]);
-}
-else if (gender == "male" && result == 1) {
-    alert(males[1]);
-  } else if (gender == "male" && result == 2) {
-    alert(males[2]);
-  } else if (gender == "male" && result == 3) {
-    alert(males[3]);
-  } else if (gender == "male" && result == 4) {
-    alert(males[4]);
-  } else if (gender == "male" && result == 5) {
-    alert(males[5]);
-  } else if (gender == "male" && result == 6) {
-    alert(males[6]);
-  } else if (gender == "female" && result == 0) {
-    alert(females[0]);
-  } else if (gender == "female" && result == 1) {
-    alert(females[1]);
-  } else if (gender == "female" && result == 2) {
-    alert(females[2]);
-  } else if (gender == "female" && result == 3) {
-    alert(females[3]);
-  } else if (gender == "female" && result == 4) {
-    alert(females[4]);
-  } else if (gender == "female" && result == 5) {
-    alert(females[5]);
-  } else if (gender == "female" && result == 6) {
-    alert(females[6]);
-    else {
-        alert("null");
-      }
+let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+let namesOfFemale =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
+function validateForm() {
+    let year = document.forms["kanName"]["year"].value;
+    let month = document.forms["kanName"]["month"].value;
+    let day = document.forms["kanName"]["day"].value;
+    if (year == "" || month == "" || day == "") {
+        console.log()
+      alert("Kind fill in all the details correctly");
+      return false;
+    }else{
+        getUserInput()
     }
-    
-  
-    
-    
+  }
+var getUserInput = function() {
+    let inputYear = document.getElementById("birthYear").value
+    let intYear = parseInt(inputYear)
+    let inputMonth = document.getElementById("birthMonth").value
+    let intMonth = parseInt(inputMonth)
+    let inputDay = document.getElementById("birthDay").value
+    let intDay = parseInt(inputDay)
+    let inputGender = document.getElementById("gender").value
+    let akanName;
+    if(intDay <= 0 || intDay> 31){
+        alert("Enter correct day")
+    }
+    if(intMonth <=0 || intMonth>12){
+        alert("Enter correct month")
+    }
+    let day = new Date(intYear+"-"+intMonth+"-"+inputDay).getDay()
+    if(inputGender ==="Male"){
+        akanName = namesOfMale[day]
+        alert("Your Akan name is " + akanName + ". Because you were born on "+ daysOfTheWeek[day])
+    }else if (inputGender === "Female"){
+        akanName = namesOfFemale[day]
+        alert("Hey!Your Akan name is " + akanName + ". Because you were born on "+ daysOfTheWeek[day] + ".Have a good day!")
+    }
+}
